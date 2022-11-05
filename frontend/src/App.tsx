1d5026d1e281b0110b1c934f036599ee8bb5ad68
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-11-05 17:22:47 +0500

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import { useState, useEffect } from 'react'
 import { socket } from './socket/socket'
 
 
 function App() {
-  useEffect(() => {
-    socket.emit('chat', {message:'1234'})
-    socket.on('chat', (e)=>{
-      console.log(e);
-    })
-
-  },[])
+  socket.emit('chat', {message:'1234'});
+  
+  socket.on('chat', (e)=>{
+    console.log(e);
+  });
   return (
     <div className="App">
       
